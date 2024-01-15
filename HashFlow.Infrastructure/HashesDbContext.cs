cae445c3cb03ca1cb6e2a849165e3fa9fa4f512c
rosmorkl <rostyslavmorwork@gmail.com>
2024-01-15 21:43:20 +0200

    add DbContext

---
+++ HashFlow.Infrastructure/HashesDbContext.cs
@@ -0,0 +1,15 @@
+﻿
+using HashFlow.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HashFlow.Infrastructure;
+
+public class HashesDbContext : DbContext
+{
+    public HashesDbContext(DbContextOptions<HashesDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Hash> Hashes { get; set; } = null!;
+}
---
+++ HashFlow.Infrastructure/Migrations/20240115153536_Initial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HashFlow.Infrastructure.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Hashes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Sha1 = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hashes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Hashes");
+        }
+    }
+}
